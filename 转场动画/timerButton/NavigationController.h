f70669650153e7ecf33207f0125445d04ea3682d
郭朝顺 <657257897@qq.com>
2021-11-11 19:22:50 +0800

    [comments]   补充注释 [jira_id] none [version]

--- 转场动画/timerButton/NavigationController.h
+++ 转场动画/timerButton/NavigationController.h
@@ -12,6 +12,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NavigationController : UINavigationController
 
+/// 一个坑, 注意这个属性的命名,一定不可以是interactiveTransition
+/// 一开始起名叫interactiveTransition, 但是手势拖动时100%会发生crash, 后面发现UINavigationController有一个私有变量叫interactiveTransition,是bool类型, 子类重写导致setTransitionAnimation设置了一个bool值,触发了野指针崩溃
 @property (nonatomic, strong) MyPercentDrivenInteractiveTransition *transitionAnimation;
 
 @end
